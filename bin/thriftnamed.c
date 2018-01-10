b5a0aabd41be4774b57accc19520c9825749952d
zhaoweikid <zhaoweikid@gmail.com>
2018-01-10 17:05:18 +0800

    debug

--- bin/thriftnamed.c
+++ bin/thriftnamed.c
@@ -5,16 +5,67 @@
 int conn_read_body(zcAsynIO *conn, const char *data, int len)
 {
     int namelen = 0;
+	char tmp[len*5];
+
+	zc_format_hex(tmp, data, len);
+
+	ZCDEBUG("data: %s\n", tmp);
 
     memcpy(&namelen, data+4, 4); 
     namelen = htob32(namelen);
     ZCDEBUG("namelen:%d", namelen);
 
     char name[256] = {0};
-    strncpy(name, data+8, namelen);
+    //strncpy(name, data+8, namelen);
+    //ZCDEBUG("name:%s", name);
 
-    ZCDEBUG("name:%s", name);
-	ZCDEBUG("data:%s", data);
+	int i;
+
+	char type = 0;
+	int  seqid = 0;
+	short id = 0;
+	char  v1;
+	short v2;
+	int   v3;
+	long long v4;
+	double vd;
+	char   vs[1024];
+
+	i = zc_thrift_read_msg_begin(data, name, &type, &seqid);
+	ZCDEBUG("name:%s type:%d seqid:%d", name, type, seqid);
+	while (1) {
+		i = zc_thrift_read_field_begin(&data[i], NULL, &type, &id);
+		ZCDEBUG("type:%d, id:%d, i:%d", type, id, i);
+		if (type == ZC_THRIFT_STOP)
+			break;
+
+		switch(type) {
+		case ZC_THRIFT_BOOL:
+			i = zc_thrift_read_bool(&data[i], &v1);
+			break;
+		case ZC_THRIFT_BYTE:
+			i = zc_thrift_read_byte(&data[i], &v1);
+			break;
+		case ZC_THRIFT_I16:
+			i = zc_thrift_read_i16(&data[i], &v2);
+			break;
+		case ZC_THRIFT_I32:
+			i = zc_thrift_read_i32(&data[i], &v3);
+			break;
+		case ZC_THRIFT_I64:
+			i = zc_thrift_read_i64(&data[i], &v4);
+			break;
+		case ZC_THRIFT_DOUBLE:
+			i = zc_thrift_read_double(&data[i], &vd);
+			break;
+		case ZC_THRIFT_STRING:
+			i = zc_thrift_read_binary(&data[i], vs, &v3);
+			break;
+		default:
+			break;
+		}
+	}
+
 
     return ZC_OK;
 }
